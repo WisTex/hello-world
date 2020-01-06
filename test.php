6689f6f96d1eae865d4616ccb93d87c53eee29be
Scott M. Stolz <sstolz@wistex.com>
2020-01-05 20:29:16 -0600

    Tesing NetBeans Editor
    Just testing out making changes in the Apache Netbeans IDE.

--- test.php
+++ test.php
@@ -1,3 +1,17 @@
+<html>
+    
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta style="font:Georgia;">
+</head>
+
+
+<body>
+
+<h1>Heading</h1>
+
+<p><b>A test document for GitHub.</b></p>
+
 <p>This is a test.</p>
 
 <p><?php echo "something" . $test; ?></p>
@@ -5,3 +19,22 @@
 <p>Should we commit this change to GIT?</p>
 
 <p>Making another change.</p>
+
+<?php
+
+    $test = "test";
+    echo $test;
+    if ($test == "something") {
+        echo "something";
+    }
+
+?>
+
+<p>Something posted here.</p>
+
+<input type="button" value="something" name="somthing_name" />
+
+</body>
+
+</html>
+
